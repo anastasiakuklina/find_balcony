bf5e0a2573d13c30108c29d783f160fa274123e9
Anastasia Kuklina <anastasijakuklina@gmail.com>
2021-10-22 16:59:58 +0500

    add hangling for no way

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ class GameplayForm(FlaskForm):
                           (-3, 'Юг'),
                           (3, 'Север')
                       ], render_kw={'class': 'form-control'})
-    number_steps = IntegerField('Сколько шагов хотите сделать?', validators=[DataRequired(), NumberRange(min=1, max=2)],
+    number_steps = IntegerField('Сколько шагов хотите сделать? ( Не больше двух )', validators=[DataRequired(), NumberRange(min=1, max=2)],
                                 default=1,
                                 render_kw={'class': 'form-control'})
     submit = SubmitField('В путь!', render_kw={'class': 'btn btn-primary'})
@@ -55,17 +55,20 @@ def gameplay(room):
         room_id = room_id + (form.way.data * form.number_steps.data)
         if room_id in rooms:
             if room_id < 6:
-                flash('Вы находитесь в комнате: {room}'.format(room=rooms[room_id][1]))
+                # flash('Вы находитесь в комнате: {room}'.format(room=rooms[room_id][1]))
                 return redirect(url_for("gameplay", room=rooms[room_id][0]))
             else:
                 return redirect(url_for("finish"))
+        else:
+            flash('Туда пути нет. Поменяйте направление или количество шагов.')
+            return render_template('gameplay.html', form=form, room=room)
     flash('Вы находитесь в комнате: {room}'.format(room=rooms[room_id][1]))
     return render_template('gameplay.html', form=form)
 
 class FinishForm(FlaskForm):
     rate = IntegerField('Поставьте оценку игре от 0 до 10', validators=[DataRequired(), NumberRange(min=0, max=10)],
                         render_kw={'class': 'form-control'})
-    feedback = StringField('Напишите отзыв, если вам есть что сказать', render_kw={'class': 'form-control'})
+    feedback = StringField('Напишите отзыв, если вам есть что сказать. (На самом деле, отзыв не сохранится. Оценка тоже)', render_kw={'class': 'form-control'})
     submit = SubmitField('Отправить оценку', render_kw={'class': 'btn btn-primary'})
 
 
